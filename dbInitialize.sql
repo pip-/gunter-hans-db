4c8c7541da27aa5238f51f38507e7f9f5acb4e0d
spradlch <spradlingchadwick@yahoo.com>
2015-11-16 12:53:22 -0600

    Update dbInitialize.sql

--- dbInitialize.sql
+++ dbInitialize.sql
@@ -1,14 +1,14 @@
 //creates tables
 CREATE TABLE transaction
 (
-  //all transactions are sales
+  //all transactions are sales, unless we need to implement transactins with suppliers
   transactionID INT PRIMARY KEY,
   transactionTimestamp TIMESTAMP,
   subTotal FLOAT, //before discount, tax, gratuity
   totalPrice FLOAT, //subtotal+tax+gratuity-discount
   totalTax FLOAT,
   gratuity FLOAT,
-  receiptNumber VARCHAR,
+  receiptNumber VARCHAR(20),
   amountPaid FLOAT,
   changeDue FLOAT,
   registerNumber SMALLINT,
@@ -21,21 +21,57 @@ CREATE TABLE customer //previously 'customer_type'
 (
   //aggregate of payment_method, card_type, customer_type...et c
   customerID INT PRIMARY KEY,
-  paymentMethod VARCHAR , //credit, debit, cash, check
-  customerType VARCHAR, //walkin, takeout, drivethrough, delivery
-  cardName VARCHAR, //'N/A' for cardless, 'John Jones'
-  cardType VARCHAR, //'N/A' for cardless, 'Visa' 
-  bankAuthorization VARCHAR, //needed? also, customer is only bag of money so this goes here
+  paymentMethod VARCHAR(10) , //credit, debit, cash, check
+  customerType VARCHAR(10), //walkin, takeout, drivethrough, delivery
+  cardName VARCHAR(15), //'N/A' for cardless, 'John Jones'
+  cardType VARCHAR(15), //'N/A' for cardless, 'Visa' 
+  bankAuthorization VARCHAR(15), //needed? also, customer is only bag of money so this goes here
   lastFourCardDigits SMALLINT //needed? nah
 ) engine=INNODB ;
 
 CREATE TABLE employee
 (
-  employeeName VARCHAR,
+  employeeName VARCHAR(25),
   employeeID INT PRIMARY KEY
 ) engine=INNODB ;
 
-/*CREATE TABLE payment_method
+CREATE TABLE menu_item
+(
+  menuItemID INT PRIMARY KEY,
+  transactionID INT REFERENCES transaction(transactionID),
+  consumerPrice FLOAT,
+  costToProduce FLOAT,
+  description VARCHAR(50),
+  brand VARCHAR(20), //needed?
+  supplier VARCHAR(20), //needed?
+  category VARCHAR(20), //drink, food
+  drinkSubtype VARCHAR(20), //alcohol-whiskey, nonalcohol-soda [alcohol/non]-[alsoholType/drinkType]
+  itemTax FLOAT,
+  discount FLOAT,
+  upcBarcode INT //needed? for cost analysis maaaybe
+) engine=INNODB ;
+
+CREATE TABLE menu_item_in_transaction
+(
+  //if cost needs to be taken into account, include transaction type : buyFromSupplier....
+  //many-to-many between menu_item and transaction tables
+  transactionID INT REFERENCES transaction(transactionID),
+  menuItemID INT REFERENCES menu_item(menuItemID),
+  PRIMARY KEY(transactionID, menuItemID),
+  quantity SMALLINT //food by item (easy), drink by ounce (convert info from user input into oz)
+) engine=INNODB ;
+
+/*CREATE TABLE brand
+(
+  //needed? no
+) engine=INNODB ;
+
+CREATE TABLE menu_item_category
+(
+  //needed? no
+) engine=INNODB ;
+
+CREATE TABLE payment_method
 (
   //needed?
 ) engine=INNODB ;
@@ -49,39 +85,3 @@ CREATE TABLE operation_type
 (
   type VARCHAR, //there is only 'sale' in the csv...other potential types???
 ) engine=INNODB ;*/
-
-CREATE TABLE menu_item
-(
-  menuItemID INT PRIMARY KEY,
-  transactionID REFERENCES transaction(transactionID),
-  consumerPrice FLOAT,
-  costToProduce FLOAT,
-  description VARCHAR,
-  brand VARCHAR, //needed?
-  supplier VARCHAR, //needed?
-  category VARCHAR, //drink, food
-  drinkSubtype, //alcohol-whiskey, nonalcohol-soda [alcohol/non]-[alsoholType/drinkType]
-  itemTax FLOAT,
-  discount FLOAT,
-  upcBarcode INT //needed? for cost analysis maaaybe
-) engine=INNODB ;
-
-/*CREATE TABLE brand
-(
-  //needed? no
-) engine=INNODB ;
-
-CREATE TABLE menu_item_category
-(
-  //needed? no
-) engine=INNODB ;*/
-
-CREATE TABLE menu_item_in_transaction
-(
-  //if cost needs to be taken into account, include transaction type : buyFromSupplier....
-  //many-to-many between menu_item and transaction tables
-  transactionID INT REFERENCES transaction(transactionID),
-  menuItemID INT REFERENCES menu_item(menuItemID),
-  PRIMARY KEY(transactionID, menuItemID),
-  quantity SMALLINT //food by item (easy), drink by ounce (convert info from user input into oz)
-) engine=INNODB ;
