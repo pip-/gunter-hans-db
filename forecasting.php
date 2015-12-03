4f37787f85763f78084be97bdfd54fed63c603cb
Philip Gilbreth <pgilbreth@gmail.com>
2015-12-03 16:08:13 -0600

    Updated forecasting

--- forecasting.php
+++ forecasting.php
@@ -30,48 +30,46 @@
 			</div>
 
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-12">
 				<h3 class="text-center">Daily Forecasting</h3>
 				<hr/>
              <?php
 				$template = '
-				<div class="panel col-md-4 text-center">
-					<div class="panel-heading"><h4>%s</h4></div>
+				<div class="panel col-md-3 text-center">
+					<div class="panel-heading"><h4>%s(%s)</h4></div>
 					<div class="panel-body"><h2>%s</h2></div>
 				</div>';
 				$daily_forecasting  = '';
 
-            $link = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME) or die ("Connection Error " . mysqli_error($link));
-				$sql = "SELECT tendered_amount, returns FROM transaction WHERE DATEDIFF(STR_TO_DATE(?, '%d/%m/%Y'), date(time))=0";
+				$link = mysqli_connect(HOST, USER, PASSWORD, DBNAME) or die ("Connection Error " . mysqli_error($link));
+				$sql = "SELECT sum(tendered_amount), sum(returns), date_format(time, '%Y-%m-%d') FROM transaction WHERE weekday(time)=weekday(?) GROUP BY date_format(time, '%Y-%m-%d')";
 
-				$week = array(date('Y-m-d', strtotime('-1 week')), date('Y-m-d', strtotime('-2 week')), date('Y-m-d', strtotime('-3 week')));
-				$week_sale = array(0, 0, 0);
+				$week = array(date('Y-m-d', strtotime('+1 day')), date('Y-m-d', strtotime('+2 day')), date('Y-m-d', strtotime('+3 day')), date('Y-m-d', strtotime('+4 day')), date('Y-m-d', strtotime('+5 day')), date('Y-m-d', strtotime('+6 day')), date('Y-m-d', strtotime('+7 day')));
+				$week_sale = array(0, 0, 0, 0, 0, 0, 0);
+                $week_sale_count = array(0, 0, 0, 0, 0, 0, 0);
 
-				for ($i=0; $i<3; $i++) {
+				for ($i=0; $i<7; $i++) {
 					if ($stmt = mysqli_prepare($link, $sql)) {
 						mysqli_stmt_bind_param($stmt, "s", $week[$i]) or die("bind param");
 						
 						if(mysqli_stmt_execute($stmt)) {
 							$result = mysqli_stmt_get_result($stmt);
 							while ($row = mysqli_fetch_row($result)) {
-							        $week_sale[$i] = $row[0]-$row[1];
+							    $week_sale[$i] += $row[0]-$row[1];
+                                $week_sale_count[$i]++;
 							}
 						}
 					}
-					$daily_forecasting .= sprintf($template, $week[$i], $week_sale[$i]);
+                    if ($week_sale_count[$i] != 0) {
+                        $week_sale[$i] = round($week_sale[$i]/$week_sale_count[$i], 2);
+                    }
+					$daily_forecasting .= sprintf($template, $week[$i], date('D', strtotime($week[$i])) . '.', $week_sale[$i]);
 				}
 				echo $daily_forecasting;
-
-				$today = $week_sale[2] + ($week_sale[2]-$week_sale[0])/2;
-				if ($today < 0) {
-					$today = 0;
-				}
-			    echo '<div class="text-center"><canvas id="dailychart"></canvas></div>';
-
-				echo '<div class="text-center"><h4>Daily sale: ' . $today .'</h4></div>';
+			    echo '<div class="text-center col-md-12"><canvas id="dailychart" height="300" width="800"></canvas></div>';
 			?>
-			</div>
-			<div class="col-md-6">
+			     </div>
+			     <div class="col-md-12">
 				<h3 class="text-center">Monthly Forecasting</h3>
 				<hr/>
 				<?php
@@ -82,51 +80,32 @@
 				</div>';
 				$month_forecasting  = '';
 
-				$sql = "SELECT sum(tendered_amount), sum(returns) FROM transaction WHERE DATEDIFF(STR_TO_DATE(?, '%Y-%m-%d'), date(time))<=0 AND DATEDIFF(STR_TO_DATE(?, '%Y-%m-%d'), date(time))>=0";
-				$last_year = date('Y-m', strtotime('-1 year'));
-				$last_year_start = $last_year.'-1';
-				$last_year_end = $last_year.'-31';
-				$last_year_sale = 0;
+				$sql = "SELECT sum(tendered_amount), sum(returns), DATE_FORMAT(time, '%Y') FROM transaction WHERE MONTH(time)=MONTH(?) GROUP BY DATE_FORMAT(time, '%Y')";
+				$past_month = array(date('Y-m-d', strtotime('+1 month')), date('Y-m-d', strtotime('+2 month')), date('Y-m-d', strtotime('+3 month')), date('Y-m-d', strtotime('+4 month')), date('Y-m-d', strtotime('+5 month')), date('Y-m-d', strtotime('+6 month')), date('Y-m-d', strtotime('+7 month')), date('Y-m-d', strtotime('+8 month')), date('Y-m-d', strtotime('+9 month')), date('Y-m-d', strtotime('+10 month')), date('Y-m-d', strtotime('+11 month')), date('Y-m-d', strtotime('+12 month')));
+				$past_month_sale = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                $past_month_sale_count = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-				if ($stmt = mysqli_prepare($link, $sql)) {
-					mysqli_stmt_bind_param($stmt, "ss", $last_year_start, $last_year_end) or die("bind param");
-					
-					if(mysqli_stmt_execute($stmt)) {
-						$result = mysqli_stmt_get_result($stmt);
-						while ($row = mysqli_fetch_row($result)) {
-						    $last_year_sale = $row[0]-$row[1];
-						}
-					}
-				}
-				$month_forecasting .= sprintf($template, $last_year, $last_year_sale);
-
-				$sql = "SELECT sum(tendered_amount), sum(returns) FROM transaction WHERE DATEDIFF(STR_TO_DATE(?, '%Y-%m-%d'), date(time))<0 AND DATEDIFF(STR_TO_DATE(?, '%Y-%m-%d'), date(time))>=0";
-				$past_month = array(date('Y-m', strtotime('-4 month')), date('Y-m', strtotime('-3 month')), date('Y-m', strtotime('-2 month')), date('Y-m', strtotime('-1 month')));
-				$past_month_sale = array(0, 0, 0);
-
-				for ($i=0; $i<3; $i++) {
+				for ($i=0; $i<12; $i++) {
 					if ($stmt = mysqli_prepare($link, $sql)) {
-						$past_month_start = $past_month[$i].'-31';
-						$past_month_end = $past_month[$i+1].'-31';
-						mysqli_stmt_bind_param($stmt, "ss", $past_month_start, $past_month_end) or die("bind param");
+						mysqli_stmt_bind_param($stmt, "s", $past_month[$i]) or die("bind param");
 						
 						if(mysqli_stmt_execute($stmt)) {
 							$result = mysqli_stmt_get_result($stmt);
 							while ($row = mysqli_fetch_row($result)) {
-							    $past_month_sale[$i] = $row[0]-$row[1];
+                                echo $row[0];
+							    $past_month_sale[$i] += $row[0]-$row[1];
+                                $past_month_sale_count[$i]++;
 							}
 						}
 					}
-					$month_forecasting .= sprintf($template, $past_month[$i+1], $past_month_sale[$i]);
+                    if ($past_month_sale_count[$i] != 0) {
+                        $past_month_sale[$i] = round($past_month_sale[$i]/$past_month_sale_count[$i], 2);
+                    }
+                    $past_month[$i] = date('Y-m', strtotime($past_month[$i]));
+					$month_forecasting .= sprintf($template, $past_month[$i], $past_month_sale[$i]);
 				}
 				echo $month_forecasting;
-
-				$month = ($past_month_sale[2] + ($past_month_sale[2]-$past_month_sale[0])/2 + $last_year_sale)/2;
-				if ($month < 0) {
-					$month = 0;
-				}
-                echo '<div class="text-center"><canvas id="monthchart"></canvas></div>';
-				echo '<div class="text-center"><h4>Monthly sale: ' . $month .'</h4></div>';
+                echo '<div class="text-center col-md-12"><canvas id="monthchart" height="300" width="800"></canvas></div>';
 				?>
 			</div>
 		</div>
@@ -134,7 +113,7 @@
     <script type="text/javascript">
     <?php
     $dailydata_template = 'var dailydata = {
-        labels: ["%s", "%s", "%s"],
+        labels: ["%s", "%s", "%s", "%s", "%s", "%s", "%s"],
         datasets: [
             {
                 fillColor: "rgba(220,220,220,0.2)",
@@ -143,18 +122,18 @@
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [%s, %s, %s]
+                data: [%s, %s, %s, %s, %s, %s, %s]
             }
         ]
     };';
-    $dailydata = sprintf($dailydata_template, $week[0], $week[1], $week[2],
-        $week_sale[0], $week_sale[1], $week_sale[2]);
+    $dailydata = sprintf($dailydata_template, $week[0], $week[1], $week[2], $week[3], $week[4], $week[5], $week[6],
+        $week_sale[0], $week_sale[1], $week_sale[2], $week_sale[3], $week_sale[4], $week_sale[5], $week_sale[6]);
     echo $dailydata;
     echo 'var dailyctx = document.getElementById("dailychart").getContext("2d");
     var dailyChart = new Chart(dailyctx).Line(dailydata);';
 
     $monthdata_template = 'var monthdata = {
-        labels: ["%s", "%s", "%s"],
+        labels: ["%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s"],
         datasets: [
             {
                 fillColor: "rgba(220,220,220,0.2)",
@@ -163,12 +142,11 @@
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [%s, %s, %s]
+                data: [%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s]
             }
         ]
     };';
-    $monthdata = sprintf($monthdata_template, $past_month[0], $past_month[1], $past_month[2],
-        $past_month_sale[0], $past_month_sale[1], $past_month_sale[2]);
+    $monthdata = sprintf($monthdata_template, $past_month[0], $past_month[1], $past_month[2], $past_month[3], $past_month[4], $past_month[5], $past_month[6], $past_month[7], $past_month[8], $past_month[9], $past_month[10], $past_month[11], $past_month_sale[0], $past_month_sale[1], $past_month_sale[2], $past_month_sale[3], $past_month_sale[4], $past_month_sale[5], $past_month_sale[6], $past_month_sale[7], $past_month_sale[8], $past_month_sale[9], $past_month_sale[10], $past_month_sale[11]);
     echo $monthdata;
     echo 'var monthctx = document.getElementById("monthchart").getContext("2d");
     var monthChart = new Chart(monthctx).Line(monthdata);';
